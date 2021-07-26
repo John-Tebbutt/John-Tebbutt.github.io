232d4efdc89896e8e3c91fd822ad1d7810ba8e42
John-Tebbutt <31164947+John-Tebbutt@users.noreply.github.com>
2021-07-26 10:12:35 +0100

    Add files via upload

---
+++ scripts/rollover.js
@@ -0,0 +1,36 @@
+
+  // Script for that performs rollovers
+
+	function mouseOverfb()
+		{
+		document.getElementById("jtfb").src ="images/fb-high.gif";
+		}
+	function mouseOutfb()
+		{
+		document.getElementById("jtfb").src ="images/fb.gif";
+		}
+	function mouseOvertwit()
+		{
+		document.getElementById("jttwit").src ="images/tw-high.gif";
+		}
+	function mouseOuttwit()
+		{
+		document.getElementById("jttwit").src ="images/tw.gif";
+		}
+	function mouseOverem()
+		{
+		document.getElementById("jtem").src ="images/em-high.gif";
+		}
+	function mouseOutem()
+		{
+		document.getElementById("jtem").src ="images/em.gif";
+		}
+	function mouseOverprint()
+		{
+		document.getElementById("jtprint").src ="images/print-high.gif";
+		}
+	function mouseOutprint()
+		{
+		document.getElementById("jtprint").src ="images/print.gif";
+		}
+
